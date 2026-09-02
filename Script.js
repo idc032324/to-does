b4950ab324696103b8514bafb7aa5bbc69171a90
idc032324 <neumannmichael658@gmail.com>
2026-09-02 09:40:00 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,10 +1,10 @@
-const button = document.getElementById("designButton");
+const button = document.getElementById("d1");
 const theme = document.getElementById("theme");
 
 button.addEventListener("click", () => {
-    if (theme.getAttribute("href") === "style1.css") {
-        theme.setAttribute("href", "style2.css");
-    } else {
+    if (theme.getAttribute("href") === "style.css") {
         theme.setAttribute("href", "style1.css");
+    } else {
+        theme.setAttribute("href", "style.css");
     }
 });
